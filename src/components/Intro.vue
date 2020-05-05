60a8279d3180773baff10e90158cf79c6802b7b0
Devan Pellow <pellod01@bitbucket.org>
2020-05-05 22:41:07 +0200

    added intro

---
+++ src/components/Intro.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="text-4xl text-white flex justify-center" v-text="name"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      name: "Devan Pellow",
+    };
+  },
+};
+</script>
+
+<style></style>
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="h-screen bg-gray-600">
+  <div class="h-screen bg-black">
+    <Intro></Intro>
     <ProjectList></ProjectList>
   </div>
 </template>
 
 <script>
 import ProjectList from '../components/ProjectList.vue'
-
+import Intro from '../components/Intro.vue'
 export default {
   components: {
-    ProjectList
+    ProjectList,
+    Intro
   }
 }
 </script>

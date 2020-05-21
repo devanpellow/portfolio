c5340cb1276504ed2a1fe299c7e245da7b43bd56
Devan Pellow <pellod01@bitbucket.org>
2020-05-21 06:43:59 +0200

    improved caluclator logic

--- src/components/CrazyCalculator.vue
+++ src/components/CrazyCalculator.vue
@@ -5,54 +5,68 @@
         <div
           class="h-16 w-full text-right text-white text-5xl bg-black border-none px-2"
         >
-          {{ display }}
+          {{ current || "" }}
         </div>
       </transition>
       <div class="flex flex-row">
-        <div :class="['border w-1/2 h-16 text-2xl']">
-          <button @click="shuffle()" class="w-full h-full">C</button>
+        <div class="border w-1/2 h-16 text-2xl">
+          <button @click="clear()" value="clr" class="w-full h-full">
+            C
+          </button>
         </div>
-        <div :class="['border w-1/4 h-16 text-2xl']">
-          <button @click="shuffle()" class="w-full h-full">del</button>
+        <div class="border w-1/4 h-16 text-2xl">
+          <button @click="del()" value="del" class="w-full h-full">
+            del
+          </button>
         </div>
-        <div :class="['border w-1/4 h-16 text-2xl']">
-          <button @click="shuffle()" class="w-full h-full">÷</button>
+        <div class="border w-1/4 h-16 text-2xl">
+          <button @click="divide()" value="div" class="w-full h-full">
+            ÷
+          </button>
         </div>
       </div>
       <div class="flex">
-        <transition-group class="w-3/4 justify-center flex flex-wrap" name="slide-up" appear>
+        <transition-group
+          class="w-3/4 justify-center flex flex-wrap"
+          name="slide-up"
+          appear
+        >
           <div
             v-for="number in numbers"
-            :class="['border h-16 w-16 text-2xl']"
+            class="border h-16 w-16 text-2xl"
             :key="number.value"
           >
-            <button @click="shuffle()" class="w-full h-full">
+            <button @click="append(number)" class="w-full h-full">
               {{ number.label }}
             </button>
           </div>
-          
         </transition-group>
         <div class="flex flex-col">
-          <div
-            v-for="operator in operators"
-            :class="['border h-16 w-16 text-2xl']"
-            :key="operator.value"
-          >
-            <button @click="shuffle()" class="w-full h-full">
-              {{ operator.label }}
+          <div class="border h-16 w-16 text-2xl">
+            <button @click="times()" value="div" class="w-full h-full">
+              ×
+            </button>
+          </div>
+          <div class="border h-16 w-16 text-2xl">
+            <button @click="minus()" value="div" class="w-full h-full">
+              −
+            </button>
+          </div>
+          <div class="border h-16 w-16 text-2xl">
+            <button @click="add()" value="div" class="w-full h-full">
+              +
             </button>
           </div>
         </div>
       </div>
       <div>
-        
-        <div :class="['-my-16 border h-16 w-16 text-2xl']">
-          <button @click="shuffle()" class="w-full h-full">
+        <div class="-my-16 border h-16 w-16 text-2xl">
+          <button @click="dot('.')" value="." class="w-full h-full">
             .
           </button>
         </div>
         <div :class="['border ml-32 h-16 w-32 text-2xl']">
-          <button @click="shuffle()" class="w-full h-full">
+          <button @click="equal()" value="sum" class="w-full h-full">
             =
           </button>
         </div>
@@ -65,7 +79,11 @@
 export default {
   data: function() {
     return {
-      display: "",
+      previous: null,
+      current: "",
+      operator: null,
+      operatorClicked: false,
+
       numbers: [
         {
           label: "7",
@@ -103,52 +121,72 @@ export default {
           label: "3",
           value: 3,
         },
-
         {
           label: "0",
           value: 0,
         },
       ],
-      operators: [
-        {
-          label: "×",
-          value: "mul",
-        },
-        {
-          label: "−",
-          value: "sub",
-        },
-        {
-          label: "+",
-          value: "add",
-        },
-        // {
-        //   label: "=",
-        //   value: "sum",
-        // },
-      ],
     };
   },
   methods: {
-    inputTrigger: function() {
-      this.shuffle();
-      // if (c.value === "=") {
-      //   this.evaluate();
-      // } else if (c.value === "C") {
-      //   this.clearDisplay();
-      // } else {
-      //   this.display += c.value;
-      // }
+    append(number) {
+      if (number.value >= 0 || number.value < 9) {
+        this.shuffle();
+
+        if (this.operatorClicked) {
+          this.current = "";
+          this.operatorClicked = false;
+        }
+        this.current = `${this.current}${number.value}`;
+      } else {
+        if (this.operatorClicked) {
+          this.current = "";
+          this.operatorClicked = false;
+        }
+        this.current = `${this.current}${number}`;
+      }
     },
-    shuffle: function() {
+    dot() {
+      if (this.current.indexOf(".") === -1) {
+        this.append(".");
+      }
+    },
+    setPrevious() {
+      this.previous = this.current;
+      this.operatorClicked = true;
+    },
+    divide() {
+      this.operator = (a, b) => a / b;
+      this.setPrevious();
+    },
+    times() {
+      this.operator = (a, b) => a * b;
+      this.setPrevious();
+    },
+    minus() {
+      this.operator = (a, b) => a - b;
+      this.setPrevious();
+    },
+    add() {
+      this.operator = (a, b) => a + b;
+      this.setPrevious();
+    },
+    equal() {
+      this.current = `${this.operator(
+        parseFloat(this.previous),
+        parseFloat(this.current)
+      )}`;
+      this.previous = null;
+    },
+
+    shuffle() {
       this.numbers.sort(() => Math.random() - 0.5);
-      // this.buttons.sort((a, b) => a.number - b.number);
     },
-    evaluate: function() {
-      this.display = eval(this.display).substring(0, 8);
+    clear() {
+      this.current = "";
     },
-    clearDisplay: function() {
-      this.display = "";
+    del() {
+      this.current = this.current.substring(0, this.current.length - 1);
     },
   },
 };
